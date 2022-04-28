01150479ef288ccf2f7d6e76276dd2a8fb3db3ab
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-28 14:07:02 +0800

    edited User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # Relations
+  has_secure_password
+
+  # Validations
+  validates :first_name, presence: true, length: { in: 1..100 }
+  validates :last_name, presence: true, length: { in: 1..100 }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, uniqueness: true
 end
