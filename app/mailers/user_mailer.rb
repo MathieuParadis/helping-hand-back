a64eeeea717b421107cab6bdb6dc0a89b68a9537
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-06 12:37:55 +0800

    Edited mailer and created mail views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,19 @@
 class UserMailer < ApplicationMailer
+  def welcome_email(user)
+    @user = user
+    @url  = 'https://helpping-hand.herokuapp.com/' 
+    mail(to: @user.email, subject: 'Welcome to Helping Hand')
+  end
+
+  def reset_password_email(user)
+    @user = user 
+    @url ="https://helpping-hand.herokuapp.com/reset-password/#{user.reset_password_token}"
+    mail(to: @user.email, subject: "Reset your password (Helping Hand)") 
+  end
+
+  def goodbye_email(user)
+    @user = user
+    @url  = 'https://helpping-hand.herokuapp.com/' 
+    mail(to: @user.email, subject: "That's only a goodbye")
+  end
 end
