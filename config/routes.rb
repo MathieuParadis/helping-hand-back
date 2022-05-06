c9bb75a850f439be7daee655ed81a2b86caa5712
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-06 10:54:54 +0800

    edited passwords controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, except: :create
   post 'signup' => 'users#create'
   post 'signin' => 'users#login'
+  post 'forgotten-password' => 'password#forgot'
+  post 'reset-password' => 'password#reset'
 end
