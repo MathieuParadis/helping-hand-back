c8aecf426db18fc191b8f5ef182759a72e9cd3b3
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-06 10:57:10 +0800

    edited user model by adding method about forgotten and reseeting password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,26 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { in: 2..100 }
   validates :last_name, presence: true, length: { in: 2..100 }
   validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, uniqueness: true
+
+  def generate_password_token!
+    self.reset_password_token = generate_token
+    self.reset_password_sent_at = Time.now.utc
+    save!
+  end
+   
+  def password_token_valid?
+  (self.reset_password_sent_at + 4.hours) > Time.now.utc
+  end
+  
+  def reset_password!(password)
+  self.reset_password_token = nil
+  self.password = password
+  save!
+  end
+  
+  private
+   
+  def generate_token
+  SecureRandom.hex(10)
+  end
 end
