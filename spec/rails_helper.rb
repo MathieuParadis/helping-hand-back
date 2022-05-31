ce6b2353ab10c3b7ade27c3be275848a63abcbe2
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-31 13:16:09 +0800

    working on tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require './support/api_helpers'
-require './support/user_helpers'
+require 'support/api_helpers'
+require 'support/user_helpers'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
