e1582d22e7709ac55c5655242a6b0fdc2e35c385
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-06-03 07:58:00 +0800

    config action cable for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://helpping-hand-back.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://helpping-hand.herokuapp.com', /https:\/\/helpping-hand.herokuapp.*/ ]
+  # config.action_cable.allowed_request_origins = [ 'https://helpping-hand.herokuapp.com', /https:\/\/helpping-hand.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
